90d053b0bb4ce2e087f8f950bb49a600350bea0f
Guilherme Goncalves <guilherme.p.gonc@gmail.com>
2014-04-08 10:00:10 -0300

    Accept json input too.

--- commands.c
+++ commands.c
@@ -17,88 +17,64 @@
 #define CMD_SHOW_POI       "show"
 #define CMD_RATE           "rate"
 
+static char *cmd_set_position(client_t *client, json_t *args);
 static char *cmd_list_all_poi(void);
-static char *cmd_show_poi(const char *poi);
-static char *cmd_rate_poi(const char *poi, const char *rating);
+static char *cmd_show_poi(json_t *args);
+static char *cmd_rate_poi(json_t *args);
 
-static char **strsplit(const char *str, char sep, int maxparts);
-static char *strstrip(const char *str, int len);
-static void strlist_free(char **strlist);
 static int make_list_from_cols(void *userdata, int argc, char **argv, char **cols);
-
-// XXX this also strips each part of the string after splitting
-static char **
-strsplit(const char *str, char sep, int maxparts) {
-    int parts = 1;
-    for (const char *s = str; *s && (maxparts < 0 || parts < maxparts); s++) {
-        parts += (*s == sep);
-    }
-
-    const char *s = str;
-    char **ret = calloc(parts + 1, sizeof(char *));
-    for (int i = 0; i < parts; i++) {
-        const char *end = strchr(s, sep);
-        if (!end || i == maxparts - 1) end = s + strlen(s);
-        ret[i] = strstrip(s, end - s);
-        s = end + 1;
-    }
-
-    return ret;
-}
-
-static char *
-strstrip(const char *str, int len) {
-    const char *start = str;
-    const char *end = str + len - 1;
-
-    while (isspace(*start)) start++;
-    while (end > start && isspace(*end)) end--;
-
-    return strndup(start, end - start + 1);
-}
-
-static void
-strlist_free(char **strlist) {
-    int i = 0;
-    while (true) {
-       if (strlist[i]) {
-           free(strlist[i]);
-       } else break;
-
-       i++;
-    }
-}
 
 char *
 process_commands(client_t *client, const char *request) {
+    const char *command;
+    json_t *args = NULL;
     char *retval = NULL;
-    char **request_parts = strsplit(request, ' ', 2);
-    char *command = request_parts[0];
-    char *args = request_parts[1];
 
-    char **argv = NULL;
-    if (args) argv = strsplit(args, ',', -1);
+    json_t *jreq = json_loads(request, 0, NULL);
+    if (!request) {
+        retval = strdup("bad request.");
+        goto bad_request;
+    }
+
+    int ok;
+    ok = json_unpack(jreq, "{s:s, s:o}", "command", &command, "args", &args);
+    if (ok < 0) {
+        retval = strdup("bad request.");
+        goto bad_request;
+    }
 
     if (!strcmp(command, CMD_POSITION)) {
-        if (!client_set_position(client, atof(argv[0]), atof(argv[1]))) {
-            return strdup("no coverage for your position!");
-        }
-        retval = strdup("ack");
+        retval = cmd_set_position(client, args);
     } else if (!strcmp(command, CMD_LIST_ALL_POI)) {
         retval = cmd_list_all_poi();
     } else if (!strcmp(command, CMD_SHOW_POI)) {
-        if (!argv) retval = strdup("missing argument");
-        else retval = cmd_show_poi(argv[0]);
+        retval = cmd_show_poi(args);
     } else if (!strcmp(command, CMD_RATE)) {
-        if (!argv || !argv[1]) retval = strdup("missing argument");
-        else retval = cmd_rate_poi(argv[0], argv[1]);
+        retval = cmd_rate_poi(args);
     } else {
         retval = strdup("invalid command");
     }
 
-    strlist_free(request_parts);
-    if (argv) strlist_free(argv);
+bad_request:
+    // no need to decref args, as we the 'o' format
+    // specifier doesn't incref
+    if (jreq) json_decref(jreq);
+
     return retval;
+}
+
+static char *
+cmd_set_position(client_t *client, json_t *args) {
+    double latitude, longitude;
+    if (json_unpack(args, "[f, f]", &latitude, &longitude) < 0) {
+        return strdup("bad arguments.");
+    }
+
+    if (!client_set_position(client, latitude, longitude)) {
+        return strdup("no coverage for your position!");
+    }
+
+    return strdup("ack");
 }
 
 static int
@@ -137,7 +113,12 @@ make_object_from_cols(void *userdata, int argc, char **argv, char **cols) {
 }
 
 static char *
-cmd_show_poi(const char *poi) {
+cmd_show_poi(json_t *args) {
+    const char *poi;
+    if (json_unpack(args, "[s]", &poi) < 0) {
+        return strdup("bad arguments.");
+    }
+
     json_t *object = json_object();
     db_run("select * from places where name = %Q",
             make_object_from_cols, object, poi);
@@ -148,8 +129,23 @@ cmd_show_poi(const char *poi) {
 }
 
 static char *
-cmd_rate_poi(const char *poi, const char *rating) {
-    db_run("update places set rating = %Q where name = %Q", NULL, NULL, rating,
-            poi);
-    return cmd_show_poi(poi);
+cmd_rate_poi(json_t *args) {
+    const char *poi;
+    double rating;
+    char *rating_str = NULL;
+
+    if (json_unpack(args, "[s, f]", &poi, &rating) < 0) {
+        return strdup("bad arguments.");
+    }
+
+    // the db expects rating to be a string, so normalize it now
+    // that we know for sure it's a double
+    asprintf(&rating_str, "%.2f", rating);
+    db_run("update places set rating = %Q where name = %Q", NULL, NULL,
+            rating_str, poi);
+    free(rating_str);
+
+    // remove rating so we only send the poi to cmd_show_poi
+    json_array_remove(args, 1);
+    return cmd_show_poi(args);
 }
