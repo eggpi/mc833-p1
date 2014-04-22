21f4046cfa39ad4acb5c3b12c575f6b3a1a90ee4
ramongtx <ramongtx@gmail.com>
2014-04-21 21:54:20 -0300

    SQL creation file added

---
+++ places.sql
@@ -0,0 +1,4 @@
+CREATE TABLE places(name string, latitude double, longitude double, rating double, category string);
+INSERT INTO "places" VALUES('pepe loco',5.0,5.0,5.2,'restaurant');
+INSERT INTO "places" VALUES('joe & leo''s',10.0,12.0,6.6,'restaurant');
+INSERT INTO "places" VALUES('delta blues bar',5.0,4.2,6.0,'bar');
